99a371c1d857bbf3b2402967149974c638d85e21
Iara <loure.iara.s@gmail.com>
2023-11-20 12:56:56 -0300

    apost13(projeto): Resolucao exercicio 1.4.2

--- proje_apost13.sql
+++ proje_apost13.sql
@@ -1,5 +1,35 @@
 -- Exercício
 
+-- 1.4.2 Exibe o percentual de estudantes de cada sexo.
+
+DO $$
+DECLARE
+    v_percentual_feminino FLOAT;
+    v_percentual_masculino FLOAT;
+BEGIN
+    CALL sp_percentual_sexo(v_percentual_feminino, v_percentual_masculino);
+    RAISE NOTICE 'Percentual do sexo feminino: %', v_percentual_feminino;
+    RAISE NOTICE 'Percentual do sexo masculino: %', v_percentual_masculino;
+END;
+$$
+
+
+
+DROP PROCEDURE IF EXISTS sp_percentual_sexo;
+CREATE OR REPLACE PROCEDURE sp_percentual_sexo(
+    OUT percentual_feminino FLOAT,
+    OUT percentual_masculino FLOAT
+) LANGUAGE plpgsql
+AS $$
+BEGIN
+    -- Calcular o percentual de estudantes do sexo feminino
+    SELECT COUNT(*) * 100.0 / (SELECT COUNT(*) FROM tb_performance) INTO percentual_feminino FROM tb_performance WHERE gender = 1;
+    -- Calcular o percentual de estudantes do sexo masculino
+    SELECT COUNT(*) * 100.0 / (SELECT COUNT(*) FROM tb_performance) INTO percentual_masculino FROM tb_performance WHERE gender = 2;
+END;
+$$
+
+
 --1.4.1 Exibe o número de estudantes maiores de idade
 
 DO $$
