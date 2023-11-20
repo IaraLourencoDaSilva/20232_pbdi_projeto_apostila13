75bae8c57722fd67e71cb78e89ddd07023bdd1b1
Iara <loure.iara.s@gmail.com>
2023-11-20 11:29:11 -0300

    apost13(projeto): Resolução exercio 1.4.1

--- proje_apost13.sql
+++ proje_apost13.sql
@@ -1,11 +1,35 @@
-SELECT * FROM tb_performance;
+-- Exercício
 
-CREATE TABLE tb_performance(
-	cod_per SERIAL PRIMARY KEY,
-	AGE INT,
-	GENDER INT,
-	SALARY INT,
-	PREP_EXAM INT,
-	NOTES INT,
-	GRADE INT
-);
+--1.4.1 Exibe o número de estudantes maiores de idade
+
+DO $$
+DECLARE
+	v_count INT;
+BEGIN
+	CALL sp_idade_maior(v_count);
+	RAISE NOTICE 'Total: %', v_count;
+END;
+$$
+
+DROP PROCEDURE IF EXISTS sp_idade_maior;
+CREATE OR REPLACE PROCEDURE sp_idade_maior(
+	OUT result_count INT
+) LANGUAGE plpgsql
+AS $$
+BEGIN 
+	SELECT COUNT(*) INTO result_count FROM tb_performance WHERE age > 1;
+END;
+$$
+
+
+-- SELECT * FROM tb_performance;
+
+-- CREATE TABLE tb_performance(
+-- 	cod_per SERIAL PRIMARY KEY,
+-- 	AGE INT,
+-- 	GENDER INT,
+-- 	SALARY INT,
+-- 	PREP_EXAM INT,
+-- 	NOTES INT,
+-- 	GRADE INT
+-- );
